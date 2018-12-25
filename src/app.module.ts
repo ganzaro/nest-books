71731be14df7dfa0ea9d2f9bb097f9d966a138e9
ganzaro <ganzaro.af@gmail.com>
2018-12-25 19:18:28 +0000

    error handling, interceptor

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,22 @@
 import { Module } from '@nestjs/common';
 import {TypeOrmModule} from '@nestjs/typeorm';
+import { APP_FILTER } from '@nestjs/core';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BookModule } from './book/book.module';
+import { HttpErrorFilter } from './shared/http-error.filter';
+import { from } from 'rxjs';
 
 @Module({
-  imports: [TypeOrmModule.forRoot() ,BookModule],
+  imports: [TypeOrmModule.forRoot() , BookModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpErrorFilter,
+    },
+  ],
 })
 export class AppModule {}
