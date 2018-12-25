62226b0f4b00b554e3c4c59685179aeae375fc5a
ganzaro <ganzaro.af@gmail.com>
2018-12-25 10:14:00 +0000

    created book module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import {TypeOrmModule} from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BookModule } from './book/book.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot() ,BookModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/book/book.service.spec.ts
@@ -0,0 +1,16 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { BookService } from './book.service';
+
+describe('BookService', () => {
+  let service: BookService;
+  
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [BookService],
+    }).compile();
+    service = module.get<BookService>(BookService);
+  });
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
